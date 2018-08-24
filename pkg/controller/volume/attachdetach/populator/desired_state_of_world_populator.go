5b2fa68241ccc79949eebec2828de83558155310
Hemant Kumar <hekumar@redhat.com>
2018-08-24 15:18:02 -0400

    Use uniform function for pod add/remove

--- pkg/controller/volume/attachdetach/populator/desired_state_of_world_populator.go
+++ pkg/controller/volume/attachdetach/populator/desired_state_of_world_populator.go
@@ -165,13 +165,13 @@ func (dswp *desiredStateOfWorldPopulator) findAndAddActivePods() {
 	dswp.timeOfLastListPods = time.Now()
 
 	for _, pod := range pods {
-		if volutil.IsPodTerminated(pod, pod.Status) {
-			// Do not add volumes for terminated pods
-			continue
+		// Use same function we use on podAdd event. This will ensure we are processing deleted
+		// pods correctly.
+		volumeActionFlag := util.DetermineVolumeAction(pod, dswp.desiredStateOfWorld, true /* add volume */)
+		if volumeActionFlag {
+			util.ProcessPodVolumes(pod, true,
+				dswp.desiredStateOfWorld, dswp.volumePluginMgr, dswp.pvcLister, dswp.pvLister)
 		}
-		util.ProcessPodVolumes(pod, true,
-			dswp.desiredStateOfWorld, dswp.volumePluginMgr, dswp.pvcLister, dswp.pvLister)
-
 	}
 
 }
