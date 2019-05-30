kubernetes/api

Auto-generated code for volume limits API
Kubernetes-commit: b90ca5b2a7a79abf230f01d657b501ca6c254733

storage/v1beta1/types_swagger_doc_generated.go
@@ -72,6 +72,7 @@ var map_CSINodeDriver = map[string]string{
 	"name":         "This is the name of the CSI driver that this object refers to. This MUST be the same name returned by the CSI GetPluginName() call for that driver.",
 	"nodeID":       "nodeID of the node from the driver point of view. This field enables Kubernetes to communicate with storage systems that do not share the same nomenclature for nodes. For example, Kubernetes may refer to a given node as \"node1\", but the storage system may refer to the same node as \"nodeA\". When Kubernetes issues a command to the storage system to attach a volume to a specific node, it can use this field to refer to the node name using the ID that the storage system will understand, e.g. \"nodeA\" instead of \"node1\". This field is required.",
 	"topologyKeys": "topologyKeys is the list of keys supported by the driver. When a driver is initialized on a cluster, it provides a set of topology keys that it understands (e.g. \"company.com/zone\", \"company.com/region\"). When a driver is initialized on a node, it provides the same topology keys along with values. Kubelet will expose these topology keys as labels on its own node object. When Kubernetes does topology aware provisioning, it can use this list to determine which labels it should retrieve from the node object and pass back to the driver. It is possible for different nodes to use different topology keys. This can be empty if driver does not support topology.",
+	"allocatable":  "allocatable represents the volume resources of a node that are available for scheduling.",
 }
 
 func (CSINodeDriver) SwaggerDoc() map[string]string {
@@ -184,6 +185,15 @@ var map_VolumeError = map[string]string{
 
 func (VolumeError) SwaggerDoc() map[string]string {
 	return map_VolumeError
+}
+
+var map_VolumeNodeResources = map[string]string{
+	"":      "VolumeNodeResources is a set of resource limits for scheduling of volumes.",
+	"count": "Maximum number of unique volumes managed by the CSI driver that can be used on a node. A volume that is both attached and mounted on a node is considered to be used once, not twice. The same rule applies for a unique volume that is shared among multiple pods on the same node. If this field is nil, then the supported number of volumes on this node is unbounded.",
+}
+
+func (VolumeNodeResources) SwaggerDoc() map[string]string {
+	return map_VolumeNodeResources
 }
 
 // AUTO-GENERATED FUNCTIONS END HERE

gnufied/kubernetes

staging/src/k8s.io/api/storage/v1beta1/types_swagger_doc_generated.go
@@ -72,6 +72,7 @@ var map_CSINodeDriver = map[string]string{
 	"name":         "This is the name of the CSI driver that this object refers to. This MUST be the same name returned by the CSI GetPluginName() call for that driver.",
 	"nodeID":       "nodeID of the node from the driver point of view. This field enables Kubernetes to communicate with storage systems that do not share the same nomenclature for nodes. For example, Kubernetes may refer to a given node as \"node1\", but the storage system may refer to the same node as \"nodeA\". When Kubernetes issues a command to the storage system to attach a volume to a specific node, it can use this field to refer to the node name using the ID that the storage system will understand, e.g. \"nodeA\" instead of \"node1\". This field is required.",
 	"topologyKeys": "topologyKeys is the list of keys supported by the driver. When a driver is initialized on a cluster, it provides a set of topology keys that it understands (e.g. \"company.com/zone\", \"company.com/region\"). When a driver is initialized on a node, it provides the same topology keys along with values. Kubelet will expose these topology keys as labels on its own node object. When Kubernetes does topology aware provisioning, it can use this list to determine which labels it should retrieve from the node object and pass back to the driver. It is possible for different nodes to use different topology keys. This can be empty if driver does not support topology.",
+	"allocatable":  "allocatable represents the volume resources of a node that are available for scheduling.",
 }
 
 func (CSINodeDriver) SwaggerDoc() map[string]string {
@@ -184,6 +185,15 @@ var map_VolumeError = map[string]string{
 
 func (VolumeError) SwaggerDoc() map[string]string {
 	return map_VolumeError
+}
+
+var map_VolumeNodeResources = map[string]string{
+	"":      "VolumeNodeResources is a set of resource limits for scheduling of volumes.",
+	"count": "Maximum number of unique volumes managed by the CSI driver that can be used on a node. A volume that is both attached and mounted on a node is considered to be used once, not twice. The same rule applies for an unique volume that is shared among multiple pods on the same node. If this field is nil, then the supported number of volumes on this node is unbounded.",
+}
+
+func (VolumeNodeResources) SwaggerDoc() map[string]string {
+	return map_VolumeNodeResources
 }
 
 // AUTO-GENERATED FUNCTIONS END HERE